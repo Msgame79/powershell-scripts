7e395bcca0913fb02d942b1eef1e4690346130c6
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-02-15 00:26:45 +0900

    テンプレートを作った

---
+++ template.ps1
@@ -0,0 +1,32 @@
+# 文字エンコードの指定
+chcp 65001
+
+<#
+エラーメッセージの処理
+Continue(デフォルト):表示しつつ続ける
+SilentlyContinue:表示せずに続ける
+#>
+$ErrorActionPreference = 'Continue'
+
+# 使う変数の初期値(これを書くことで編集を容易にできる)
+# 同時に型を指定すると何を入れればいいのか明瞭になる
+[string]$DefaultDirectory = $PSScriptRoot
+
+# 関数一覧
+function versioncheck { # 特定以上のバージョンを使うよう指示
+    param (
+        [int]$a = 5
+    )
+    [string]$b = ""
+    if ($PSVersionTable.PSVersion.Major -lt $versionrequirement) {
+        $b = "Please run this ps1 file on PowerShell $versionrequirement or newer`nEnter to exit"
+        Read-Host
+        Start-Process "https://github.com/PowerShell/PowerShell/releases/latest"
+        Exit
+    }
+    return $b
+}
+
+# メイン処理
+Set-Location $DefaultDirectory
+versioncheck 7
