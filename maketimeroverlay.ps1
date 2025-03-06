2ac336552a46d542a95729ec145521a3c446f47a
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-03-07 01:54:22 +0900

    変更を同期しなくていいファイルはパスを.gitignoreに書けばいいんですね

--- maketimeroverlay.ps1
+++ maketimeroverlay.ps1
@@ -1,17 +1,50 @@
 chcp 65001
 $ErrorActionPreference = 'SilentlyContinue'
+<#
+このps1ファイルはバージョン6以降で動作します。versioncheckが使用できないのでここに書きますがPowerShellのバージョン6以降をGitHubかMSStoreより入手してください。
+URL: https://github.com/PowerShell/PowerShell/releases/latest
+#>
 
 # 変数一覧(変更可能)
-[string]$defaultfolder = "C:\Users\Msgames79\Videos" # デフォルト: $PSScriptRoot
+[string]$defaultfolder = ".\av1test" # デフォルト: $PSScriptRoot
 [string]$vencodesetting = "" # デフォルト: "-c:v libx264 -qp 21" フィルターをかけるので"-c:v copy"は使えない
-[string]$vencodesetting = "" # デフォルト: "-c:a " デフォルトではあえて再エンコードするように書いているが、できるなら"-c:a copy"が良い
+[string]$aencodesetting = "" # デフォルト: "-c:a " デフォルトではあえて再エンコードするように書いているが、できるなら"-c:a copy"が良い
 [string]$outputextension = "mp4" # デフォルト: "mp4" デフォルトがmp4向けのエンコード設定のため。ただし上のエンコード設定によっては変える必要あり、あとここで編集させているのはすぐ上にエンコード設定があるから
 <#
-いろんなエンコードメモ
-ffplayがあるから基本的にどんなコーデックでもいい
-それなら
+目的別いろんなエンコードメモ
+再生できればいいからとにかく容量を小さくしたい場合
+hevc+Opus,mkv Container
+[string]$vencodesetting = "-c:v libx265 -qp 18"
+[string]$aencodesetting = "-c:a libopus -b:a 96k"
+[string]$outputextension = "mkv"
 
+互換性が欲しい場合
+h264+aac, mp4 container
+[string]$vencodesetting = "-c:v libx264 -qp 18"
+[string]$aencodesetting = "-c:a aac -q:a 1"
+[string]$outputextension = "mp4"
+VP9+Opus, webm container
+[string]$vencodesetting = "-c:v libvpx-vp9"
+[string]$aencodesetting = "-c:a libopus -b:a 96k"
+[string]$outputextension = "webm"
 
+可逆圧縮したい場合
+h264(lossless)+alac, mp4 container
+[string]$vencodesetting = "-c:v libx264 -qp 0"
+[string]$aencodesetting = "-c:a flac"
+[string]$outputextension = "mp4"
+utvideo+flac, mkv container (ファイルサイズがでかくなる)
+[string]$vencodesetting = "-c:v utvideo"
+[string]$aencodesetting = "-c:a flac"
+[string]$outputextension = "mkv"
+ffv1+flac, mkv container
+[string]$vencodesetting = "-c:v ffv1 -level 3"
+[string]$aencodesetting = "-c:a flac"
+[string]$outputextension = "mkv"
+VP9(lissless)+Opus(非可逆圧縮だがWebm側が可逆圧縮の音声コーデックをサポートしていない), webm container(エンコードがめちゃ遅い)
+[string]$vencodesetting = "-c:v libvpx-vp9 -lossless 1"
+[string]$aencodesetting = "-c:a libopus -b:a 128k"
+[string]$outputextension = "webm"
 #>
 
 # 変数一覧(変更不可能)
@@ -19,17 +52,6 @@ ffplayがあるから基本的にどんなコーデックでもいい
 [array]$inputfilelist = @()
 [string]$inputfilename = ""
 
-function versioncheck { # 特定以上のメジャーバージョンを使うよう指示
-    param ( # 引数一覧(コンマで区切るのを忘れない!)
-        [int]$a = 5 # 引数を取らなかったときの初期値
-    )
-    if ($PSVersionTable.PSVersion.Major -lt $a) {
-        Write-Host "バージョン${a}以上のPowerShellで実行してください`nEnterで終了"
-        Read-Host
-        Start-Process "https://github.com/PowerShell/PowerShell/releases/latest"
-        Exit
-    }
-}
 ffmpeg -version | Out-Null
 if (-not $?) {
     Write-Host "ffmpegを実行できません`nEnterで終了"
@@ -53,7 +75,6 @@ if (-not $?) {
 #メイン処理
 if ($IsWindows) {
     Set-Location $defaultfolder
-    versioncheck 5
     # 動画選択
     $inputfilelist = Get-ChildItem -Name | Where-Object {$_ -match "^.+\.(mp4|mov|mkv|avi|webm|mpg|flv|wmv|ogv|asf)$"}
     do {
@@ -61,8 +82,8 @@ if ($IsWindows) {
         $inputfilelist
         $inputfilename = Read-Host -Prompt "動画ファイルを選択"
     } until ($inputfilename -in $inputfilelist)
+    Start-Process -FilePath "ffplay" -ArgumentList "-hide_banner -loglevel -8 -window_title ""フレーム確認"" -loop 0 -i $inputfilename -vf ""pad=w=iw:h=ih+75:x=0:y=75,drawtext=fontsize=70:fontcolor=white:fontfile=c\\:/windows/fonts/cour.ttf:y_align=font:text='%{eif\:ceil(t*$(ffprobe -i $inputfilename -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=r_frame_rate" | Invoke-Expression))\:u\:0}'""" -NoNewWindow
 } else {
     Write-Host "現在このps1ファイルはWindowsでのみ動作します`nEnterで終了"
     Read-Host
-    exit
 }
